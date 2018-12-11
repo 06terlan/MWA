b583fa348ce4bfb30efb314fcc82aba7fbb6f64b
Tarlan <06terlan91@gmail.com>
2018-12-10 22:39:56 -0600

    hw11

--- Homework 11/App/src/app/app.component.ts
+++ Homework 11/App/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'App';
+	ComponentCounterValue:number = 0;
+  	
+  	counterChange(e){
+  		this.ComponentCounterValue = e;
+  	}
 }
